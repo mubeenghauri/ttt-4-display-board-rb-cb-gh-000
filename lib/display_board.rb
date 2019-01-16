d2acf631d6ae406e218067c0061fd05f2d88a117
Mubeen Ghauri <ghauri.mubeen@gmail.com>
2019-01-16 08:28:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,13 @@
 
 
 def display_board(board)
-  
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
+
+
+display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "])
